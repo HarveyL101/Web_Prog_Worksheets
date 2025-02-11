c5d678d981aa8694b342f23175f2bbe234791825
HarveyL101 <harveylopez101@gmail.com>
2025-02-11 18:46:54 +0000

    adding fetch101 + some answered problems.

---
+++ fetch101-main/index.js
@@ -0,0 +1,43 @@
+/*
+ * This is index.js
+ *
+ * NB: all code you write this year should use strict mode, so
+ * we've enabled that by default with the first line of code.
+ */
+
+'use strict';
+
+async function showMessage(elem, url) {
+  const response = await fetch(url);
+  const content = await response.text();
+  elem.textContent = content;
+}
+
+async function showList(elem, url) {
+  const response = await fetch(url);
+  const content = await response.json();
+
+  content.forEach(item => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    elem.appendChild(listItem);
+  });
+}
+
+async function startShowingMessage(elem, url) {
+  setInterval(async () => {
+    const response = await fetch(url);
+    const content = await response.text();
+    elem.textContent = content;
+  }, 1000);
+}
+
+async function handleError(elem, url) {
+  const response = await fetch(url);
+  if(response.ok) {
+    const content = await response.text();
+    elem.textContent = content;
+  } else {
+    elem.textContent = "OH DEAR";
+  }
+}
